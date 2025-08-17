f8d557b9c77bbb9f4bfd197af0296f5066d00a98
Yash Mude <yashmude@Yashs-MacBook-Pro.local>
2025-08-17 10:35:25 -0500

    AI Chat comp add

--- src/pages/AI Chat/AIchat.tsx
+++ src/pages/AI Chat/AIchat.tsx
@@ -5,8 +5,8 @@ export default function AIchat() {
   return (
     <div>
       <PageMeta
-        title="React.js Blank Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Blank Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="AI Chat"
+        description="This is FinSight AI Chat page"
       />
       <PageBreadcrumb pageTitle="AI Chatbot" />
       <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
